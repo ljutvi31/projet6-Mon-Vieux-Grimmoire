82b1a876fbc62b6635960f12c6276acf7fa24dec
Harry Ljutvi <ljutvi.h@gmail.com>
2025-01-03 12:59:16 +0100

    database mongodb & models user & book created

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 const app = express();
 
+mongoose.connect(process.env.MONGODB_URI)
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch((error) => console.log('Connexion à MongoDB échouée !', error));
+    
 app.use(express.json());
 
 app.use((req, res, next) => {
